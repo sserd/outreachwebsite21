43e82f4c9f50f0a0621cd7606eb8705ecd22cd4e
Sujay Sreedhar <sujaysreedhar@gmail.com>
2021-08-18 14:37:49 +0530

    changes to the pages

---
+++ js/page.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+$.get("page/nav.html",function (navbar) { $("#content").prepend(navbar);});
+$.get("page/footer.html",function (footer) { $("#content").append(footer);});
+
+//$(".loadpage").load();    
+ $(function() {
+     $("#content .loadpage").each(function() {
+       $(this).load($(this).attr("data-file"));
+     });
+   });
+
+$('#pageloader').hide();
+
+});
